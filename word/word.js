0082f7766bb35a120c441c19e925ce77ec13d9f1
Carolyn Hampe <carolyn.hampe@gmail.com>
2019-06-06 16:44:12 -0400

    got words to swap out on hover

--- word/word.js
+++ word/word.js
@@ -1,7 +1,18 @@
 let alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 $( document ).ready(function() {
-    $(".letter").hover(function(hoveredEl){
-        console.log("hovered el: ", hoveredEl);
+    let letterInterval;
+    $(".letter").mouseover(function(e){
+        let hoveredLetter = $(e.target).html();
+        console.log("hovered letter: ", hoveredLetter);
+        let letterIdx = alphabet.indexOf(hoveredLetter);
+        letterInterval = setInterval(function(){
+            letterIdx = (letterIdx > (alphabet.length-1)) ? 0 : letterIdx+1;
+            console.log("letterIdx: ", letterIdx, ", letter: ", alphabet[letterIdx]);
+            $(e.target).html(alphabet[letterIdx]);
+        }, 300);
+    });
+    $(".letter").mouseout(function(e){
+        clearInterval(letterInterval);
     });
 });
